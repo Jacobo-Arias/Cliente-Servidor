9fdb2e430ae3d0ee82a277fc03c4e12ef77a2317
Jacobo Arias <jacobo.a12@utp.edu.co>
2019-08-23 12:36:22 -0500

    falta server

--- Cliente2.py
+++ Cliente2.py
@@ -1,13 +1,29 @@
 import zmq
+import os
+import hashlib
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://192.168.17.27:5555")
 
+filename = "utp.jpg"
 
-filename = "jacobo1g.txt"
+with open(filename,"rb") as filef:
+    hash_archivo = hashlib.sha256()  
+    while True: 
+        contents = filef.read(10*1024*1024)
+        if not contents:
+            break
+        hash_archivo.update(contents)  
+            
+sha_file = hash_archivo.hexdigest().encode()
+
 with open(filename,"rb") as f:
-    contents = f.read(10*1024*1024)
-    socket.send_multipart([filename.encode("utf-8"),contents])
-m = socket.recv_string()
+    while(True):
+        contents = f.read(10*1024*1024)
+        if not contents:
+            break
+        else:
+            socket.send_multipart([filename.encode("utf-8"),sha_file,contents])
+            m = socket.recv_string()
 print("Recibi: {}".format(m))
--- Server2.py
+++ Server2.py
@@ -5,13 +5,34 @@ socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
 def store(message, filename):
-	with open(filename,"wb") as f:
+	with open(filename,"a") as f:
 		f.write(message)
 
 
 while True:
-	name,m = socket.recv_multipart()
-	store(m,"f-" + str(name.decode("utf-8")))
+	names1,sha256,contents = socket.recv_multipart()
+	store(m,"f-" + str(names1.decode("utf-8")))
 	socket.send_string("chupelo")
 
 print ("Esto no deberia aparecer")
+
+
+
+'''
+files = open("ejemplo.txt","r")
+lista = files.read()
+lista = lista.split()
+for i in range(len(lista)):
+    lista[i] = lista[i].split(',')
+print (lista)
+files.close()
+
+
+lista2 = []
+for i in range(len(lista)):
+    lista2.append(",".join(lista[i]))
+lista2 = " ".join(lista2)
+print(lista2)
+files = open("ejemplo2.txt","w")
+files.write(lista2)
+'''
