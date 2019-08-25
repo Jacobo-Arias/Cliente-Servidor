3ac755762e6b69c7a5750a79793202e8e6232ceb
Jacobo Arias <jacobo.a12@utp.edu.co>
2019-08-24 23:44:02 -0500

    MEdio funciona

--- Cliente2.py
+++ Cliente2.py
@@ -4,9 +4,9 @@ import hashlib
 
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://192.168.17.27:5555")
+socket.connect("tcp://localhost:5555")
 
-filename = "utp.jpg"
+filename = "namefile.txt"
 
 with open(filename,"rb") as filef:
     hash_archivo = hashlib.sha256()  
@@ -14,7 +14,8 @@ with open(filename,"rb") as filef:
         contents = filef.read(10*1024*1024)
         if not contents:
             break
-        hash_archivo.update(contents)  
+        hash_archivo.update(contents) 
+    filef.close() 
             
 sha_file = hash_archivo.hexdigest().encode()
 
@@ -24,6 +25,7 @@ with open(filename,"rb") as f:
         if not contents:
             break
         else:
-            socket.send_multipart([filename.encode("utf-8"),sha_file,contents])
+            socket.send_multipart([filename.encode("utf-8"),sha_file,contents]) #TODO: Nombre archivo, marca hash, contenido
             m = socket.recv_string()
+    f.close()
 print("Recibi: {}".format(m))
--- Server2.py
+++ Server2.py
@@ -4,35 +4,56 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
-def store(message, filename):
-	with open(filename,"a") as f:
-		f.write(message)
+def store(filename, contents):
+	with open(filename,"ab") as f:
+		f.write(contents)
+		f.close()
 
+def read_name():
+	files = open("nombres.txt","r")
+	lista = files.read()
+	lista = lista.split()
+	for i in range(len(lista)):
+		lista[i] = lista[i].split(',')
+	files.close()
+	return lista
+
+
+def store_name(identificador):
+	lista = read_name()
+	for i in lista:
+		if i[0] == identificador [0]:
+			if i[1] == identificador[1]:
+				pass
+			else:
+				extra = '1'
+				try:
+					extra = int(identificador[-5])
+					extra += 1
+				except:
+					identificador[0] = identificador[0][:-4] + extra + identificador[0][-4:]
+	print (identificador)
+	guardar = ','.join(identificador)
+	lista2 = []
+	for i in range(len(lista)):
+		lista2.append(",".join(lista[i]))
+	lista2 = " ".join(lista2)
+	lista2 = lista2 + ' ' + guardar	
+	files = open("nombres.txt","w")
+	files.write(lista2)
+	files.close()
+	return 1
 
 while True:
-	names1,sha256,contents = socket.recv_multipart()
-	store(m,"f-" + str(names1.decode("utf-8")))
-	socket.send_string("chupelo")
+	nombre, sha256,contents = socket.recv_multipart() #TODO: Nombre archivo, marca hash, contenido
+	identificador = [nombre.decode('utf-8'),sha256.decode()]
+	save = store_name(identificador)
+	if save == 0:
+		socket.send_string('Archivo ya esta en nuestro servidor')
+	else:
+		store(sha256,contents)
+		socket.send_string("chupelo")
 
 print ("Esto no deberia aparecer")
 
 
-
-'''
-files = open("ejemplo.txt","r")
-lista = files.read()
-lista = lista.split()
-for i in range(len(lista)):
-    lista[i] = lista[i].split(',')
-print (lista)
-files.close()
-
-
-lista2 = []
-for i in range(len(lista)):
-    lista2.append(",".join(lista[i]))
-lista2 = " ".join(lista2)
-print(lista2)
-files = open("ejemplo2.txt","w")
-files.write(lista2)
-'''
